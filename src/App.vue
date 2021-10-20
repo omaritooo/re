78b4435a49b8c7873b2ab37a6d25aede60940c30
omaritooo <omaritooo@users.noreply.github.com>
2021-10-20 19:38:12 +0200

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Model />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import { ModelStl } from "vue-3d-model";
+import Model from "./components/Model.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Model },
+};
 </script>
 
 <style>
---
+++ src/components/Model.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <model-obj :src="file" mtl="../test1.mtl"></model-obj>
+  </div>
+</template>
+<script>
+import { ModelObj } from "vue-3d-model";
+import file from "../assets/test1.obj";
+export default {
+  components: { ModelObj },
+  data() {
+    return;
+    {
+      file: file;
+    }
+  },
+  mounted: function () {
+    console.log(this.file);
+  },
+};
+</script>
